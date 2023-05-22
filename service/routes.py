963f07d2f5ad6a2b3fbd2161ea282a5667f885a0
Vivek Kumar Bind <vivekkumarbind89@gmail.com>
2023-05-22 15:11:33 -0400

    debugged routes.py

--- service/routes.py
+++ service/routes.py
@@ -65,6 +65,7 @@ def create_accounts():
 
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
+
     """
     List all Accounts
     This endpoint will list all Accounts
@@ -83,6 +84,7 @@ def list_accounts():
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
+
     """
         Reads an Account
         This endpoint will read an Account based the account_id that is requested
@@ -101,6 +103,7 @@ def get_accounts(account_id):
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
+
     """
     Update an Account
     This endpoint will update an Account based on the posted data
@@ -122,6 +125,7 @@ def update_accounts(account_id):
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
+    
     """
     Delete an Account
     This endpoint will delete an Account based on the account_id that is requested
